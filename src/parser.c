551973b82946884d576aa5aa96f5782a855141ce
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-08-01 16:06:34 +0100

    feat(build)

--- src/parser.c
+++ src/parser.c
@@ -4495,7 +4495,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_strudel(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 1,
-      .patch_version = 7,
+      .patch_version = 8,
     },
   };
   return &language;
